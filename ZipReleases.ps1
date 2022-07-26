61ddd17f4050e1e45844865181e4d14e7826009a
Daniel Odievich <daniel.odievich@snowflake.com>
2022-07-26 10:13:32 -0700

    Added Apple M1/M2 support

--- ZipReleases.ps1
+++ ZipReleases.ps1
@@ -1,4 +1,14 @@
-$zip = "C:\Program Files\7-Zip\7z.exe"
+# Get 7Zip path
+if ($PSVersionTable.Platform -eq "Win32NT")
+{
+    # Windows
+    $zip = "C:\Program Files\7-Zip\7z.exe"
+}
+elseif (($PSVersionTable.Platform -eq "Unix") -and ($PSVersionTable.OS.Contains("Darwin") -eq $true))
+{
+    # Mac
+    $zip = "~/Downloads/7z2107-mac/7zz"
+}
 
 # Get version from the project file
 $projXML = [xml](Get-Content -Path .\Snowflake.GrantReport.csproj)
@@ -11,6 +21,8 @@ cd "bin\Publish\win"
 cd "..\osx"
 & $zip a "..\..\..\..\Releases\$version\SFGrantReport.osx.$version.zip" '@..\..\..\ReleaseIncludes\listfile.osx.txt'
 
+cd "../osx-arm"
+& $zip a "../../../../Releases/$version/SFGrantReport.osx-arm.$version.zip" '@../../../ReleaseIncludes/listfile.osx.txt'
 cd "..\linux"
 & $zip a "..\..\..\..\Releases\$version\SFGrantReport.linux.$version.zip" '@..\..\..\ReleaseIncludes\listfile.linux.txt'
 
